aeebfff3ce604e33f05746482ffb35faceec5a3f
Ethan Anderson <ethan.anderson00@gmail.com>
2020-08-10 20:37:38 +0100

    Added the ability to create new quizs

--- Client/src/components/QuizBuilder/QuizBuilder.tsx
+++ Client/src/components/QuizBuilder/QuizBuilder.tsx
@@ -1,26 +1,82 @@
 import React from "react";
+import QuizContentBlock from "../QuizContentBlock/QuizContentBlock";
+import { RouteChildrenProps } from "react-router-dom";
 
-class QuizBuilder extends React.Component {
-  public state = {
+type QuizContent = {
+  question: string;
+  answer: string;
+};
+
+type QuizBuilderState = {
+  name: string;
+  desc: string;
+  quizBody: Array<QuizContent>;
+};
+
+interface QuizBuilderProps extends RouteChildrenProps {}
+
+class QuizBuilder extends React.Component<QuizBuilderProps> {
+  public state: QuizBuilderState = {
     name: "",
-    description: ""
+    desc: "",
+    quizBody: []
   };
 
   public onNameInput = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const newValue = event.currentTarget.value.trim();
+    const newValue = event.currentTarget.value;
     this.setState({ name: newValue });
   };
 
   public onDescriptionInput = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const newValue = event.currentTarget.value.trim();
-    this.setState({ description: newValue });
+    const newValue = event.currentTarget.value;
+    this.setState({ desc: newValue });
+  };
+
+  public addQuizContent = () => {
+    let newQuizBody = this.state.quizBody;
+    const newQuizContent = { question: "", answer: "" };
+    newQuizBody.push(newQuizContent);
+    this.setState({ quizBody: newQuizBody });
+  };
+
+  public updateQuizContent = (index: number, newQuizContent: QuizContent) => {
+    const newQuizBody = this.state.quizBody.map((quizContent, i) => {
+      if (i === index) {
+        return (quizContent = newQuizContent);
+      } else {
+        return quizContent;
+      }
+    });
+
+    this.setState({ quizBody: newQuizBody });
+  };
+
+  public handleSubmit = () => {
+    fetch("http://localhost:8081/createQuiz", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }).catch((error) => {
+      console.error("Error: ", error);
+    });
+
+    this.props.history.push("/");
   };
 
   public render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input placeholder="Name" value={this.state.name} onChange={this.onNameInput}></input>
-        <input placeholder="Description" value={this.state.description} onChange={this.onDescriptionInput}></input>
+        <input placeholder="Description" value={this.state.desc} onChange={this.onDescriptionInput}></input>
+        {this.state.quizBody.map((quizContent, i) => (
+          <QuizContentBlock key={i} index={i} onUpdate={this.updateQuizContent}></QuizContentBlock>
+        ))}
+        <button type="button" onClick={this.addQuizContent}>
+          Add QuizContent
+        </button>
+        <button type="submit">Submit</button>
       </form>
     );
   }
--- Client/src/components/QuizContentBlock/QuizContentBlock.tsx
+++ Client/src/components/QuizContentBlock/QuizContentBlock.tsx
@@ -1,11 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 
-class QuizContentBlock extends Component {
+type QuizContent = {
+  question: string;
+  answer: string;
+};
+
+type QuizContentBlockProps = {
+  index: number;
+  onUpdate: (key: number, newQuizContent: QuizContent) => void;
+};
+
+class QuizContentBlock extends React.Component<QuizContentBlockProps> {
+  public state = {
+    question: "",
+    answer: ""
+  };
+
+  public onQuestionInput = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const newValue = event.currentTarget.value;
+    const newQuizContent = { question: newValue, answer: this.state.answer };
+    this.props.onUpdate(this.props.index, newQuizContent);
+    this.setState({ question: newValue });
+  };
+
+  public onAnswerInput = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const newValue = event.currentTarget.value;
+    const newQuizContent = { question: this.state.question, answer: newValue };
+    this.props.onUpdate(this.props.index, newQuizContent);
+    this.setState({ answer: newValue });
+  };
+
   render() {
     return (
       <div>
-        <input placeholder="Question"></input>
-        <input placeholder="Answer"></input>
+        <input placeholder="Question" value={this.state.question} onChange={this.onQuestionInput}></input>
+        <input placeholder="Answer" value={this.state.answer} onChange={this.onAnswerInput}></input>
       </div>
     );
   }
